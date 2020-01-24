a25d3033ef90cc18c7c3799180712761d37bff05
mallikarjun math <mallikarjun.math@beyondid.com>
2020-01-24 20:34:08 +0530

    update

--- aws1.tf
+++ aws1.tf
@@ -1,4 +1,4 @@
-resource "aws_ami_from_instance" "example" {
+resource "aws_ami_from_instance" "CHTest_1a_Linux_VM" {
   name               = "terraform-example"
   source_instance_id = "i-097d721a5e365e590"
 }
