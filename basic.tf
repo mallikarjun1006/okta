8ae4f4a94764d3c3e457e38d6aca71b2fed6962d
mallikarjun math <mallikarjun.math@beyondid.com>
2020-01-27 15:17:15 +0530

    Update basic.tf

--- basic.tf
+++ basic.tf
@@ -1,4 +1,6 @@
 resource "okta_user" "test" {
+	source  = "app.terraform.io/beyondid/okta/provider"
+  version = "3.0.38"
   first_name = "TestAccterraform"
   last_name  = "Smith"
   login      = "test-acc-replace_with_uuidterraform@example.com"
