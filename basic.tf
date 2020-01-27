625269cd9ff3909a4dfe9fd7311a86983c2d2cc7
mallikarjun math <mallikarjun.math@beyondid.com>
2020-01-27 16:12:05 +0530

    aws_vpc

--- basic.tf
+++
@@ -1,8 +0,0 @@
-resource "okta_user" "test" {
-	source  = "app.terraform.io/beyondid/okta/provider"
-  version = "3.0.38"
-  first_name = "TestAccterraform"
-  last_name  = "Smith"
-  login      = "test-acc-replace_with_uuidterraform@example.com"
-  email      = "test-acc-replace_with_uuidterraform@example.com"
-}
--- main.tf
+++
@@ -1,6 +0,0 @@
-//--------------------------------------------------------------------
-// Modules
-module "okta" {
-  source  = "app.terraform.io/beyondid/okta/provider"
-  version = "3.0.38"
-}
