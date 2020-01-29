8b8e20db7d25227145c05f7a7d3ff43824cae9bd
mallikarjun math <mallikarjun.math@beyondid.com>
2020-01-29 12:57:09 +0530

    Update aws_vpc.tf

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -28,7 +28,20 @@ resource "aws_security_group" "main" {
   name        = "main"
   description = "Allow TLS inbound traffic"
   vpc_id      = "${aws_vpc.main.id}"
+ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    
+  }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]    
+  }
   
 
   tags = {
