11fc091392813f8c6d8eae41a0e06ab54aacb08e
mallikarjun math <mallikarjun.math@beyondid.com>
2020-02-28 18:32:53 +0530

    Update aws_lb_target_group.tf

--- aws_lb_target_group.tf
+++ aws_lb_target_group.tf
@@ -6,7 +6,7 @@ resource "aws_lb_target_group" "testgroup" {
 }
 
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = "${aws_lb.front_end.arn}"
+  load_balancer_arn = "${aws_lb.mainlb.arn}"
   port              = "80"
   protocol          = "HTTP"
   
