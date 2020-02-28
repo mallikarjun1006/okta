92eed34330bf6d90f34699b9f0613f8f367c46cd
mallikarjun math <mallikarjun.math@beyondid.com>
2020-02-28 18:31:33 +0530

    Update aws_lb_target_group.tf

--- aws_lb_target_group.tf
+++ aws_lb_target_group.tf
@@ -5,3 +5,14 @@ resource "aws_lb_target_group" "testgroup" {
   vpc_id   = "${aws_vpc.main.id}"
 }
 
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_lb.front_end.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.testgroup.arn}"
+  }
+}
