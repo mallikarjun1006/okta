dbf6f6e7362235bb00051ccc6d72769f59bc9ffb
mallikarjun math <mallikarjun.math@beyondid.com>
2020-03-02 18:50:25 +0530

    Update aws_eip.tf

--- aws_eip.tf
+++ aws_eip.tf
@@ -1,9 +1,11 @@
 resource "aws_eip" "eip1" {
+	name="instance1 eip"
   instance = "${aws_instance.web.id}"
   vpc      = true
 }
 
 resource "aws_eip" "eip2" {
+name="instance1 eip"
   instance = "${aws_instance.web1.id}"
   vpc      = true
 }
