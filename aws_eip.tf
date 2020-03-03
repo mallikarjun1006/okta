198a43052736b0517093585d5d5a0b9f624269b5
mallikarjun math <mallikarjun.math@beyondid.com>
2020-03-03 18:39:16 +0530

    Update aws_eip.tf

--- aws_eip.tf
+++ aws_eip.tf
@@ -12,5 +12,5 @@ resource "aws_eip" "eip2" {
 
 resource "aws_key_pair" "ec2user" {
   key_name   = "ec2user"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAxVnhMfcugV+t2IeznoyCRk9AqrUo+NPivbLU2/iMwgQmlTQ8fQLxDwpCtUn0tcxI/ur6drWmnX7akWJyxsCbDnHfKL07uI54Z+w+iJkJ/bboixSIys1tKZn3mRfdoSl4RaWSAkjjOI5vcM4Un7OH8WKeRvtQgFbOSA2gJP/+Yia9fIPynCKXXG9sdhc90TTKKPGr2b8WRr9JVjK50HLRvZfh8epK8r50tnxuzXUD6Y5WjhJhtQNKDV8ow ec2-user"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAxVnhMfcugV+t2IeznoyCRk9AqrUo+NPivbLU2/iMwgQmlTQ8fQLxDwpCtUn0tcxI/ur6drWmnX7akWJyxsCbDnHfKL07uI54Z+w+iJkJ/bboixSIys1tKZn3mRfdoSl4RaWSAkjjOI5vcM4Un7OH8WKeRvtQgFbOSA2gJP/+Yia9fIPynCKXXG9sdhc90TTKKPGr2b8WRr9JVjK50HLRvZfh8epK8r50tnxuzXUD6Y5WjhJhtQNKDV8ow+B9r4QNcVCVbbwnDwRPwgaYdBxT/iZTvYxSUZLaM6clmyXoVRxK4vCigBoZniyvo/OTqIUdqw1HKzrIjfSsu0TwzREpdw== rsa-key-20200303"
 }
