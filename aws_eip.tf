f86ab228e9494b3e4ee7a94d15773e7272c09b66
mallikarjun math <mallikarjun.math@beyondid.com>
2020-03-03 18:19:24 +0530

    Update aws_eip.tf

--- aws_eip.tf
+++ aws_eip.tf
@@ -12,5 +12,5 @@ resource "aws_eip" "eip2" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAxVnhMfcugV+t2IeznoyCRk9AqrUo+NPivbLU2/iMwgQmlTQ8fQLxDwpCtUn0tcxI/ur6drWmnX7akWJyxsCbDnHfKL07uI54Z+w+iJkJ/bboixSIys1tKZn3mRfdoSl4RaWSAkjjOI5vcM4Un7OH8WKeRvtQgFbOSA2gJP/+Yia9fIPynCKXXG9sdhc90TTKKPGr2b8WRr9JVjK50HLRvZfh8epK8r50tnxuzXUD6Y5WjhJhtQNKDV8ow"
 }
